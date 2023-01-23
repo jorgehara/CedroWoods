a9a880c25fd4a2a0144f2e316b2f562fac0b0cea
jorgehara <empresamodelo2018@gmail.com>
2023-01-23 02:55:26 -0300

    pushInitial

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { Carousel } from './Components/Carousel/Carousel'
+import { Header } from './Components/Header/Header'
+
+export default function App() {
+  return (
+   <>
+      <Header/>
+   <Carousel/>
+
+
+   </>
+  )
+}
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import './Header.css'
+
+export const Header = () => {
+  return (
+    <>
+<div className='containerHeader'>
+
+
+<div className='w-auto'>
+   <h2
+   className="bg-orange-400 bg-gradient-to-tr font-bold leading-tight mb-4 md:mb-2 md:text-4xl  text-white text-center p-2"
+   >CEDRO Woods - Fábrica de Muebles
+   </h2>
+    </div>
+    
+    <div className='w-auto'>
+    <h2
+   className="bg-orange-600 bg-gradient-to-tr font-bold leading-tight mb-4 md:mb-2 md:text-4xl text-white p-2"
+   >Contactanos al 3794-123456
+   </h2>
+   </div>
+    </div>
+    </>
+  )
+}
