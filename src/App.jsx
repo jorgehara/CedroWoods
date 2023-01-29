e6a4eeddb4049f5076f480fb4fabd1c9343ea597
jorgehara <empresamodelo2018@gmail.com>
2023-01-29 16:13:44 -0300

    Mercado Pago halfSlice

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,11 @@ import { Footer } from "./Components/Footer/Footer";
 import { Galery } from "./Components/Galeria/Galery";
 import { Header } from "./Components/Header/Header";
 import { Projects } from "./Components/Projects/Projects";
+import { Slice } from "./Components/SliceMercadoPago/Slice";
 export default function App() {
   return (
     <>
+    <Slice/>
       <Header />
       <Galery />
       <Projects />
