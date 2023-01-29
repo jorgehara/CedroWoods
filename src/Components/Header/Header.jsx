dca24846b1b7d04b964a944db851f157f1da4058
jorgehara <empresamodelo2018@gmail.com>
2023-01-28 22:03:21 -0300

    make better details of wasap

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -20,9 +20,9 @@ export const Header = () => {
    className="bg-orange-600 bg-gradient-to-tr font-bold leading-tight mb-4 md:mb-2 md:text-4xl text-white p-2 rounded-md"
    >Contactanos al 3794-123456
    </h2>
-    <a href="https://wa.me/543794350479?Somos%20CEDRO%20Woods%20realizamos%20muebles%20con%20diseños%20personalizados%20y%20a%20medida.%20Esperamos%20su%20consulta!!%20:D" target='_blank'><img src="/image/CEDROwoods/whatsapp-fill (2).svg" alt="Imagen ilustrativa de muebles entregados" /></a>
-
+    <a href="https://wa.me/543794350479?Somos%20CEDRO%20Woods%20realizamos%20muebles%20con%20diseños%20personalizados%20y%20a%20medida.%20Esperamos%20su%20consulta!!%20:D" target='_blank'><img src="/image/CEDROwoods/whatsapp-fill (3).svg" alt="Imagen ilustrativa de muebles entregados" /></a>
     </div>
+
 
 
 
