c6438aff5bd961e975b57c09d9d2adcd1e995759
jorgehara <empresamodelo2018@gmail.com>
2023-01-29 01:15:11 -0300

    Cards Succesfull

--- src/Components/Cards/Cards.jsx
+++ src/Components/Cards/Cards.jsx
@@ -4,15 +4,15 @@ import './Cards.css'
 export const Cards = ({image, description, tittle}) => {
   return (
     <>  
-    <div className="flex flex-col pt-3">
-  <div className="flex rounded-lg shadow-lg bg-white max-w-sm">
-    <a className='aImgCard' href="#!">
-      <img className='imgCard' src={image} alt=""/>
+    <div className="flex flex-col">
+  <div className="flex rounded-lg shadow-lg bg-white w-85">
+    <a className='p-0' href="#!">
+      <img className='imgCard' src={image} alt="Image referencia de trabajos realizados"/>
     </a>
-    <div className="p-1">
-      <h5 className="text-gray-900 text-xl font-medium pt-1 mb-4 mr-4">{tittle}</h5>
-      <p className="text-gray-700 text-base mb-3">{description} </p>
-      <button type="button" className=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Button</button>
+    <div className="p-0">
+      <h5 className="text-gray-900 text-base p-1">{tittle}</h5>
+      <p className="text-gray-700 text-justify text-md align-text-top w-48  p-4">{description} </p>
+      <button type="button" className=" justify-center ml-4 w-25 mb-2 bg-blue-600 text-white font-medium text-sm leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Button</button>
     </div>
   </div>
 </div>
--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -6,44 +6,49 @@ const projects = [
     {
         id: 1,
         title: "Muebles de cocina",
-        description: "Muebles de cocina personalizados a medida",
-        url: "6.jpeg"
+        description: "Muebles de cocina personalizados a medida Muebles de cocina personalizados a medidaMuebles de cocina personalizados a medida",
+        url: "6.jpeg",
+        className: "imgCard",
     },
     {
         id: 2,
         title: "Muebles de sala",
-        description: "Muebles de sala personalizados a medida",
-        url: "7.jpeg"
-    },
-    {
-        id: 3,
-        title: "Muebles de cocina",
-        description: "Muebles de cocina personalizados a medida",
-        url: "8.jpeg"
+        description: "Muebles de cocina personalizados a medida Muebles de cocina personalizados a medidaMuebles de cocina personalizados a medida",
+        url: "7.jpeg",
+        className: "imgCard",
+
     },
     {
         id: 4,
         title: "Muebles de sala",
-        description: "Muebles de sala personalizados a medida",
-        url: "9.jpeg"
+        description: "Muebles de cocina personalizados a medida Muebles de cocina personalizados a medidaMuebles de cocina personalizados a medida",
+        url: "9.jpeg",
+        className: "imgCard",
+
     },
     {
         id: 5,
         title: "Muebles de cocina",
         description: "Muebles de cocina personalizados a medida",
-        url: "10.jpeg"
+        url: "10.jpeg",
+        className: "imgCard",
+
     },
     {
         id: 6,
         title: "Muebles de sala",
         description: "Muebles de sala personalizados a medida",
-        url: "11.jpeg"
+        url: "11.jpeg",
+        className: "imgCard",
+
     },
     {
         id: 7,
         title: "Muebles de cocina",
         description: "Muebles de cocina personalizados a medida",
-        url: "12.jpeg"
+        url: "12.jpeg",
+        className: "imgCard",
+
     },
     // ... más elementos aquí
 ]
@@ -55,19 +60,20 @@ export const Projects = () => {
   return (
     <>
       <div className="flex flex-col ">
-      <div className="flex justify-center p-1 rounded-t-sm ">
+      <div className="flex justify-center p-1 rounded-sm ">
         <h2 className="bg-orange-400 bg-gradient-to-tr font-semibold  md:text-3xl  text-white text-center p-2 mt-3 ">
           Los mejores diseños y calidad en muebles personalizados</h2>
           </div>
-          <div className="flex justify-center p-1 rounded-t-sm ">
+          <div className="flex justify-center p-1 rounded-sm ">
         <h3 className="bg-orange-400 bg-gradient-to-tr font-semibold  md:text-3xl  text-white text-center p-2 "> Encontralos aquí</h3>
         </div>
 
-        <div className="flex justify-center flex-wrap mr-4 pr-1">
+        <div className=" justify-center grid grid-cols-3 p-2 gap-8 w-3/3">
         {projects.map(project => (
           <Cards
             key={project.id}
             image={`${imagePath}${project.url}`}
+            className={project.className}
             title={project.title}
             description={project.description}
             onClick={() => setSelectedProject(project.id)}
